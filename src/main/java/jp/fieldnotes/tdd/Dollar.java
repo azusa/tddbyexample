6460dd7ca02a83fd0ed359d2a148f6326abebaa3
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-03-08 21:49:27 +0900

    仮実装

---
+++ src/main/java/jp/fieldnotes/tdd/Dollar.java
@@ -0,0 +1,15 @@
+package jp.fieldnotes.tdd;
+
+public class Dollar {
+
+	public int amount = 10;
+
+	public Dollar(int amount) {
+	}
+
+	public void times(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
