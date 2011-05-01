76fd293f78159be61819af2d20cc4273c038f31c
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-01 22:07:15 +0900

    Francをつくった

---
+++ src/main/java/jp/fieldnotes/tdd/Franc.java
@@ -0,0 +1,1 @@
+package jp.fieldnotes.tdd;public class Franc {	private final int amount;	Franc(int amount) {		this.amount = amount;	}	public Franc times(int multiplier) {		return new Franc(amount * multiplier);	}	@Override	public boolean equals(Object arg0) {		Franc franc = (Franc) arg0;		return amount == franc.amount;	}}
