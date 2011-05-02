523d8a44fbe95a4a99d055133a22f10bb9862134
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-02 18:00:13 +0900

    第6章 FrancをMoneyから継承するようにした (本当はFrancから削除する前にMoneyとequalsの実装を削除しなければいけない)

--- src/main/java/jp/fieldnotes/tdd/Franc.java
+++ src/main/java/jp/fieldnotes/tdd/Franc.java
@@ -1,1 +1,1 @@
-package jp.fieldnotes.tdd;public class Franc {	private final int amount;	Franc(int amount) {		this.amount = amount;	}	public Franc times(int multiplier) {		return new Franc(amount * multiplier);	}}
+package jp.fieldnotes.tdd;public class Franc extends Money {	Franc(int amount) {		this.amount = amount;	}	public Franc times(int multiplier) {		return new Franc(amount * multiplier);	}}
