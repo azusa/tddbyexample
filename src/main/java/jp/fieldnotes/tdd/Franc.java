002d27d5c01c83a59e96998ef01703c1c54eb98d
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-04 22:13:48 +0900

    第10章 timesの実装をスーパークラスに移動した

--- src/main/java/jp/fieldnotes/tdd/Franc.java
+++ src/main/java/jp/fieldnotes/tdd/Franc.java
@@ -1,1 +1,1 @@
-package jp.fieldnotes.tdd;public class Franc extends Money {	Franc(int amount, String currency) {		super(amount, currency);	}	@Override	public Money times(int multiplier) {		return new Money(amount * multiplier, currency);	}}
+package jp.fieldnotes.tdd;public class Franc extends Money {	Franc(int amount, String currency) {		super(amount, currency);	}}
