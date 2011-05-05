46b90b76a6a5e4584c9b98799d08669339d45855
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-05 21:50:35 +0900

    MoneyTest#plusReturnsSum を通した

---
+++ src/main/java/jp/fieldnotes/tdd/Sum.java
@@ -0,0 +1,1 @@
+package jp.fieldnotes.tdd;public class Sum implements Expression {	public Sum(Money augend, Money addend) {		this.augend = augend;		this.addend = addend;	}	Money augend;	Money addend;}
