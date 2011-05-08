07b720253e1c978417af85f3786d48147daf6082
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-08 19:38:52 +0900

    rateの計算先を移動する

--- src/main/java/jp/fieldnotes/tdd/Bank.java
+++ src/main/java/jp/fieldnotes/tdd/Bank.java
@@ -1,1 +1,1 @@
-package jp.fieldnotes.tdd;public class Bank {	Money reduce(Expression source, String to) {		return source.reduce(this, to);	}	public void addRate(String string, String string2, int i) {		// TODO Auto-generated method stub	}}
+package jp.fieldnotes.tdd;public class Bank {	Money reduce(Expression source, String to) {		return source.reduce(this, to);	}	public void addRate(String string, String string2, int i) {		// TODO Auto-generated method stub	}	int rate(String from, String to) {		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;	}}
