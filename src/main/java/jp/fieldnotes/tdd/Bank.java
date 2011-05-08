3e62e6c2eb33a7a023395c13e716d1d796ea96c0
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2011-05-08 19:32:00 +0900

    2フランを1ドルに変換するテストケース

--- src/main/java/jp/fieldnotes/tdd/Bank.java
+++ src/main/java/jp/fieldnotes/tdd/Bank.java
@@ -1,1 +1,1 @@
-package jp.fieldnotes.tdd;public class Bank {	Money reduce(Expression source, String to) {		return source.reduce(to);	}}
+package jp.fieldnotes.tdd;public class Bank {	Money reduce(Expression source, String to) {		return source.reduce(to);	}	public void addRate(String string, String string2, int i) {		// TODO Auto-generated method stub	}}
